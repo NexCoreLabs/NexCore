0a98698a80d49cd15d686a168f21af9b2b5a89c9
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2025-10-25 19:59:16 +0400

    Update the official version

--- assets/js/script-thanks.js
+++ assets/js/script-thanks.js
@@ -1,18 +1,5 @@
-// function redirectToHome() {
-//     setTimeout(function() {
-//         window.location.href = 'index.html';
-//     }, 5000);
-// }
-
-const logoImg = document.getElementById('logoImg');
-const link = document.getElementsByClassName('fade');
-
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    logoImg.style.opacity = '1';
-    mainContent.style.opacity = '0';
-    
-    setTimeout(() => {
-      window.location.href = event.target.href;
+function redirectToHome() {
+    setTimeout(function() {
+        window.location.href = 'index.html';
     }, 1000);
-  });
+}
