44810f22e52408680dbf929ce0a2542aca4ad25e
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2026-03-07 15:36:08 +0400

    Update version.js

--- version.js
+++ version.js
@@ -1,13 +1,1 @@
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-const APP_VERSION = 'v2.0.0';
-=======
 const APP_VERSION = 'v2.4.0';
->>>>>>> Stashed changes
-=======
-const APP_VERSION = 'v2.3.0';
->>>>>>> f2cd9b3c88d6012d35b6282e6627af537d098a02
-=======
-const APP_VERSION = 'v2.4.0';
->>>>>>> Stashed changes
